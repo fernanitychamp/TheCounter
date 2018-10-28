1259c7686c10314e9875449c8e199569b98d5ea0
Andres Fernando <andres.fernando08@gmail.com>
2018-10-28 16:07:26 -0500

    modifcada las acciones del ControlCounter

--- src/components/ControlCounter/ControlCounter.js
+++ src/components/ControlCounter/ControlCounter.js
@@ -7,10 +7,10 @@ import Button from '../UI/Button/Button';
 
 const controlCounter = (props) => (
     <div className={Styles.ControlCounter}>
-        <Button clicked={props.clickedAction} payload={{type: 'inc'}}>Incrementar</Button>
-        <Button clicked={props.clickedAction} payload={{type: 'dec'}}>Decrementar</Button>
-        <Button clicked={props.clickedAction} payload={{type: 'add', value: 5}}>Sumar 5</Button>
-        <Button clicked={props.clickedAction} payload={{type: 'substract', value: 10}}>Restar 10</Button>
+        <Button clicked={props.clickedAction} act={{type: 'inc'}}>Incrementar</Button>
+        <Button clicked={props.clickedAction} act={{type: 'dec'}}>Decrementar</Button>
+        <Button clicked={props.clickedAction} act={{type: 'add', value: 5}}>Sumar 5</Button>
+        <Button clicked={props.clickedAction} act={{type: 'subtract', value: 10}}>Restar 10</Button>
     </div>
 )
 
